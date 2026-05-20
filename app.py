98d9aa521bf32ed027f166b7250d6bff3688772c
Norazlina <norazlinalina13@gmail.com>
2026-05-20 21:57:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -120,6 +120,12 @@ def logout():
 
 
 if __name__ == '__main__':
+    
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+else:
+    
     with app.app_context():
         db.create_all()
 
